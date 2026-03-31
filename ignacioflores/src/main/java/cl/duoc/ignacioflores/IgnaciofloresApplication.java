e1aa47eeea3b42d49a1404de54ca05de34f85ba1
naxo18 <ign.floresm@duocuc.cl>
2026-03-31 11:11:39 -0300

    Add files via upload
    primer comitt

---
+++ ignacioflores/src/main/java/cl/duoc/ignacioflores/IgnaciofloresApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ignacioflores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnaciofloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgnaciofloresApplication.class, args);
+	}
+
+}
---
+++ ignacioflores/src/test/java/cl/duoc/ignacioflores/IgnaciofloresApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ignacioflores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnaciofloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

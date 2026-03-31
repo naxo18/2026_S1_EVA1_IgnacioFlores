7e3d0a89f82c40dbaa20b2e1cbe939c148bddb79
naxo18 <ign.floresm@duocuc.cl>
2026-03-31 11:28:36 -0300

    se crean importaciones en solicitudservices
    Refactor UsuarioService to SolicitudService and add imports.

--- ignacioflores/src/main/java/services/SolicitudService.java
+++ ignacioflores/src/main/java/services/SolicitudService.java
@@ -1,5 +1,12 @@
 package services;
 
-public class UsuarioService {
 
-}
+import model.Solicitud;
+import repository.SolicitudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
